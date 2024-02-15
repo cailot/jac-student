01d045173d4d02a0150f0d59c4e101b6815c5de5
cailot <cailot@users.noreply.github.com>
2024-02-15 00:07:51 +0000

    apply BcryptPasswordEncoder

--- src/main/java/hyung/jin/seo/jae/config/JaeStudentSecurity.java
+++ src/main/java/hyung/jin/seo/jae/config/JaeStudentSecurity.java
@@ -1,12 +1,15 @@
 package hyung.jin.seo.jae.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 @EnableWebSecurity
@@ -18,9 +21,8 @@ public class JaeStudentSecurity extends WebSecurityConfigurerAdapter{
 
         @Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		// auth.userDetailsService(userDetailsService).passwordEncoder(getPasswordEncoder());
-        auth.userDetailsService(userDetailsService)
-            .passwordEncoder(NoOpPasswordEncoder.getInstance()); 
+		auth.userDetailsService(userDetailsService).passwordEncoder(getPasswordEncoder());
+                //auth.userDetailsService(userDetailsService).passwordEncoder(NoOpPasswordEncoder.getInstance()); 
 	}
 	
 	@Override
@@ -55,5 +57,10 @@ public class JaeStudentSecurity extends WebSecurityConfigurerAdapter{
                         .permitAll());
 		
 	}
+
+        @Bean
+        public PasswordEncoder getPasswordEncoder(){
+                return new BCryptPasswordEncoder();
+        }
 
 }
--- src/main/java/hyung/jin/seo/jae/service/impl/StudentAccountServiceImpl.java
+++ src/main/java/hyung/jin/seo/jae/service/impl/StudentAccountServiceImpl.java
@@ -1,8 +1,11 @@
 package hyung.jin.seo.jae.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import hyung.jin.seo.jae.dto.StudentAccount;
@@ -15,6 +18,9 @@ public class StudentAccountServiceImpl implements StudentAccountService {
 
 	@Autowired
 	private StudentRepository studentRepository;
+
+	// @Autowired
+	// private BCryptPasswordEncoder passwordEncoder;
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -33,10 +39,10 @@ public class StudentAccountServiceImpl implements StudentAccountService {
 
 	@Override
 	public void updatePassword(Long id, String password) {
-		// BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-		// String encodedPassword = passwordEncoder.encode(password);
+		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+		String encodedPassword = passwordEncoder.encode(password);
 		try{
-			studentRepository.updatePassword(id, password);
+			studentRepository.updatePassword(id, encodedPassword);
 		}catch(Exception e){
 			System.out.println("No student found");
 		}	
@@ -54,5 +60,6 @@ public class StudentAccountServiceImpl implements StudentAccountService {
 		// studentRepository.findById(id).get();	
 		return std;
 	}
+
 
 }
